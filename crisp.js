98e129d50f0332d6399b36123a3ca3110403079f
memsbdm <mehmetbdm@outlook.fr>
2026-08-10 21:09:44 +0400

    chore: remove Crisp support widget

--- crisp.js
+++
@@ -1,9 +0,0 @@
-window.$crisp = [];
-window.CRISP_WEBSITE_ID = "dbb32bb0-5bbf-4569-bfb9-3ed26b640b0c";
-
-(function loadCrisp() {
-  const script = document.createElement("script");
-  script.src = "https://client.crisp.chat/l.js";
-  script.async = true;
-  document.head.appendChild(script);
-})();
